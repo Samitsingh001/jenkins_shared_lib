39452b073b6c08c728bb73c3044d5a344fc31bfa
Samitsingh001 <samit.singh@ust.com>
2025-10-24 12:43:38 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,11 +1,6 @@
- def call(String project, String ImageTag, String hubUser){
-     withCredentials([usernamePassword(
-             credentialsId: "docker",
-             usernameVariable: "USER",
-             passwordVariable: "PASS"
-     )]) {
-         sh "docker login -u '$USER' -p '$PASS'"
-     }
-     sh "docker image push ${hubUser}/${project}:${ImageTag}"
-     sh "docker image push ${hubUser}/${project}:latest"   
- }
+def call(String project, String ImageTag, String hubUser){
+    sh """
+        docker rmi ${hubUser}/${project}:${ImageTag}
+        docker rmi ${hubUser}/${project}:latest
+    """
+}
