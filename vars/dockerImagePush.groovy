4afd0a1508d2d6ac0a98f0a4b68b1fd436d6213b
Samitsingh001 <samit.singh@ust.com>
2025-10-24 14:32:15 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -11,10 +11,21 @@
 // }
 
 
-def call(String aws_account_id, String region, String ecr_repoName){
-    
-    sh """
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    """
+def call(String aws_account_id, String region, String ecr_repoName) {
+    withCredentials([
+        string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
+        string(credentialsId: 'AWS_SECRET_KEY_ID', variable: 'AWS_SECRET_ACCESS_KEY')
+    ]) {
+        withEnv([
+            "AWS_DEFAULT_REGION=${region}",
+            "ECR_REGISTRY=${aws_account_id}.dkr.ecr.${region}.amazonaws.com",
+            "REPO_NAME=${ecr_repoName}"
+        ]) {
+            sh '''
+                aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
+                
+                docker push ${ECR_REGISTRY}/${REPO_NAME}:latest
+            '''
+        }
+    }
 }
